b4892c01d8d6a800fee515e42eb947570514c561
Anuj Yadav <106235866+anujyadav11@users.noreply.github.com>
2026-06-25 16:30:13 +0530

    chore : entities for creator store added

---
+++ src/main/java/com/example/creatorstore/entities/OrderItem.java
@@ -0,0 +1,34 @@
+package com.example.creatorstore.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price_at_purchase", nullable = false)
+    private BigDecimal priceAtPurchase;
+
+    // TODO: relations
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
